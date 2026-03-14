6971ce40bfe9f53e592324d57612445d6aed3f98
sinnedos <sinnedos@proton.me>
2026-03-14 15:16:14 -0500

    update failing script

--- database/sql/V6__add_columns_to_tables.sql
+++ database/sql/V6__add_columns_to_tables.sql
@@ -1,4 +1,9 @@
 ALTER TABLE IF EXISTS enquiries.contactmessages
-    ADD COLUMN createdby VARCHAR(10) NOT NULL,
-    ADD COLUMN updatedby VARCHAR(10) NOT NULL,
+    ADD COLUMN createdby VARCHAR(10),
+    ADD COLUMN updatedby VARCHAR(10),
+    ADD COLUMN updatedat TIMESTAMPTZ NOT NULL DEFAULT now();
+
+ALTER TABLE IF EXISTS enquiries.contactmessageresponses
+    ADD COLUMN createdby VARCHAR(10),
+    ADD COLUMN updatedby VARCHAR(10),
     ADD COLUMN updatedat TIMESTAMPTZ NOT NULL DEFAULT now();
