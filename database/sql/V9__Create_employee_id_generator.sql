a48716fd190d1f5a96ea0e80cafc16b982c0c34f
sinnedos <sinnedos@proton.me>
2026-03-15 17:42:48 -0500

    update database

---
+++ database/sql/V9__Create_employee_id_generator.sql
@@ -0,0 +1,19 @@
+-- Generate a new random employee ID 
+
+CREATE OR REPLACE FUNCTION generate_unique_employee_id()
+RETURNS VARCHAR(12) AS $$
+DECLARE
+    new_id VARCHAR(12);
+    done BOOL;
+BEGIN
+    done := FALSE;
+    WHILE NOT done LOOP
+        -- Generate a random 12-digit number as a string
+        new_id := LPAD(FLOOR(RANDOM() * 900000000000 + 100000000000)::TEXT, 12, '0');
+
+        -- Check if the generated ID already exists in the Employees table
+        EXIT WHEN NOT EXISTS (SELECT 1 FROM people.employees WHERE employeeid = new_id);
+    END LOOP;
+    RETURN new_id;
+END;
+$$ LANGUAGE plpgsql;
